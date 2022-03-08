7e721b127b7d07b04bf31a285f1e82287ac97f0e
Irwin <irwinngo@gmail.com>
2022-03-08 04:05:09 -0500

    First, still need to add randomization

---
+++ src/App.js
@@ -0,0 +1,116 @@
+
+import './App.css';
+import { functions as functionsInfo } from './components/functions.js';
+
+import { useState } from 'react';
+
+function App() {
+
+  
+
+  const [ numOfAdds, setNumOfAdds ] = useState([]);
+
+  const addNumOfAdds = () => {
+    if(numOfAdds.length < functionsInfo.length)
+      setNumOfAdds(num => [...num, 1]);
+  }
+
+  for(let i = 0; i < functionsInfo.length; i++) {
+    addNumOfAdds();
+  } 
+
+  const [ functionsHeck, setFunctionsHeck ] = useState([]);
+
+  const addDefaultFunctions = () => {
+    if(functionsHeck.length < functionsInfo.length)
+      setFunctionsHeck(num => [...num, []]);
+  }
+
+  for(let i = 0; i < functionsInfo.length; i++) {
+    addDefaultFunctions();
+  } 
+
+  const addFunctions = (event) => {
+    var newFunctions = [...functionsHeck];
+    
+    newFunctions[event.target.id][event.target.className] = event.target.value
+    
+    setFunctionsHeck(newFunctions);
+    console.log(functionsHeck);
+  }
+
+  const addCount = (functions) => {
+    var newNum = [...numOfAdds];
+
+    newNum[functions.id-1]+=1;
+
+    setNumOfAdds(newNum);
+  }
+
+  const removeCount = (event) => {
+    var newNum = [...numOfAdds];
+
+    if(newNum[event.target.id-1] > 1) 
+      newNum[event.target.id-1]-=1;
+
+      var newFunctions = [...functionsHeck];
+
+      newFunctions[event.target.id].shift();
+  
+      setFunctionsHeck(newFunctions);
+      console.log(functionsHeck);
+
+    setNumOfAdds(newNum);
+  }
+
+  const createInput = (functions) => {
+    var inputs = [];
+    let i = 0
+    for(; i < numOfAdds[functions.id-1]; i++) {
+      inputs.push(<>
+        <input id={functions.id} className={i} type="text" onChange={addFunctions}></input>
+        </>);
+    }
+    inputs.push(
+      <div style={{display: "flex", marginLeft: 5, gap: 10}}>
+        <div className="buttonFunction" style={{backgroundColor: "rgb(107, 107, 255)", boxShadow: "1px 1px 0px 0px rgb(28, 28, 163)"}} onClick={() => addCount(functions)}>Add</div>
+        <div id={functions.id} className="buttonFunction" style={{backgroundColor: "rgb(28, 28, 163)", boxShadow: "1px 1px 0px 0px rgb(157, 157, 255)"}} onClick={removeCount}>Remove</div>
+      </div>
+    )
+    return inputs;
+  }
+
+  return (
+    <div className="main">
+      <h1>Polymorphism Idea Generator</h1>
+      <h2>Input Ideas Here!</h2>
+      <form>
+        {functionsInfo.map((functions) => {
+          return (
+            <div className="container">
+              <label>{functions.function}</label>
+              <br/>
+              {createInput(functions)}
+              
+              
+              <br/>
+            </div>
+          )
+        })}
+        <div className="container">
+              <label>Number of Ideas Wanted</label>
+              <br/>
+              <input type="text"></input>
+              <br/>
+            </div>
+      </form>
+      <div className="generateButton" onClick={addFunctions}>Generate Ideas Now!</div>
+      <hr/>
+      <br/>
+
+      <h2>Generated Ideas</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/functions.js
@@ -0,0 +1,55 @@
+export const functions = [
+    {
+        id: 1,
+        function: "To prevent non-users from hearing the user",
+        numOfAdd: 1,
+    }
+    ,
+    {
+        id: 2,
+        function: "To prevent non-users from seeing the user",
+        numOfAdd: 1,
+    }
+    ,
+    {
+        id: 3,
+        function: "To indicate that the design is being used or is free at the site.",
+        numOfAdd: 1,
+    }
+    ,
+    {
+        id: 4,
+        function: "To indicate that the design is being used or is free in online sources",
+        numOfAdd: 1,
+    }
+    ,
+    {
+        id: 5,
+        function: "To lock the design when it is used by a user",
+        numOfAdd: 1,
+    }
+    ,
+    {
+        id: 6,
+        function: "To unlock the design when the user wants to",
+        numOfAdd: 1,
+    }
+    ,
+    {
+        id: 7,
+        function: "To ventilate clean air for the user",
+        numOfAdd: 1,
+    }
+    ,
+    {
+        id: 8,
+        function: "To provide seating for the user",
+        numOfAdd: 1,
+    }
+    ,
+    {
+        id: 9,
+        function: "To indicate when a health emergency occurs to the user",
+        numOfAdd: 1,
+    }
+]
