0c4e146531af9dcd9e06e5178a48cdb8a90302b4
Irwin <irwinngo@gmail.com>
2022-03-08 12:29:27 -0500

    Idea generator complete, without limiter!

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState } from 'react';
 
 function App() {
 
-  
+  const [ globalCount, setGlobalCount ] = useState(0);
 
   const [ numOfAdds, setNumOfAdds ] = useState([]);
 
@@ -33,7 +33,7 @@ function App() {
   const addFunctions = (event) => {
     var newFunctions = [...functionsHeck];
     
-    newFunctions[event.target.id][event.target.className] = event.target.value
+    newFunctions[event.target.id-1][event.target.className] = event.target.value
     
     setFunctionsHeck(newFunctions);
     console.log(functionsHeck);
@@ -65,7 +65,7 @@ function App() {
 
   const createInput = (functions) => {
     var inputs = [];
-    let i = 0
+    let i = 0;
     for(; i < numOfAdds[functions.id-1]; i++) {
       inputs.push(<>
         <input id={functions.id} className={i} type="text" onChange={addFunctions}></input>
@@ -79,6 +79,46 @@ function App() {
     )
     return inputs;
   }
+
+  const setCount = (event) => {
+    setGlobalCount(event.target.value);
+    console.log(globalCount);
+  }
+
+  const generateRandomIdeas = () => {
+    document.getElementById("generatedIdeas").innerHTML="";
+    var ideas=[];
+    addIdea(ideas, "", 0, 0);
+
+    for(let i = 0; i < ideas.length; i++) 
+      document.getElementById("generatedIdeas").innerHTML+="<p>" + ideas[i] + "</p>";
+    
+    document.getElementById("ideaCount").innerHTML=ideas.length;
+    
+  }
+
+  const addIdea = (ideas, ideaName, index, secondIndex) => {
+
+
+    if(index>=functionsHeck.length) {
+      ideas.push(ideaName);
+      return;
+    }
+
+    if(secondIndex >= functionsHeck[index].length)
+      return;
+    
+    let ideaNameNew = ideaName + functionsHeck[index][secondIndex] + " &nbsp;|&nbsp; ";
+
+    let indexOne = index+1;
+    let secondIndexOne = secondIndex+1;
+
+    addIdea(ideas, ideaNameNew, indexOne, 0);
+    addIdea(ideas, ideaName, index, secondIndexOne);
+
+  }
+
+
 
   return (
     <div className="main">
@@ -100,15 +140,19 @@ function App() {
         <div className="container">
               <label>Number of Ideas Wanted</label>
               <br/>
-              <input type="text"></input>
+              <input type="text" onChange={setCount}></input>
               <br/>
             </div>
       </form>
-      <div className="generateButton" onClick={addFunctions}>Generate Ideas Now!</div>
+      <div className="generateButton" onClick={() => generateRandomIdeas()}>Generate Ideas Now!</div>
       <hr/>
       <br/>
 
       <h2>Generated Ideas</h2>
+      <h3>Count: <span id="ideaCount"></span></h3>
+      <div id="generatedIdeas">
+
+      </div>
     </div>
   );
 }
